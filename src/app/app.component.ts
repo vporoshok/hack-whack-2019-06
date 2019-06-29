0be2a3f062fc0df9ea8e6977661578a6661d1e82
Evgeniy Bastrykov <vporoshok@gmail.com>
2019-06-29 10:37:41 +0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hw-root',
+  templateUrl: './app.component.html',
+  styles: []
+})
+export class AppComponent {
+  title = 'hack-whack';
+}
